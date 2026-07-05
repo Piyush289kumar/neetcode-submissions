4f05402719e81d662ea8d3bf30535c2712bf3193
Piyush Shailendra Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2026-07-05 19:17:04 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l = 0, m = 0, r = nums.size()-1;
+
+        while(m <= r){
+            if(nums[m] == 0){
+                swap(nums[m++], nums[l++]);
+            }else if(nums[m] == 2){
+                swap(nums[m], nums[r--]);
+            }else{
+                m++;
+            }
+        }
+    }
+};
